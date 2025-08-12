8a7c8d3b05841dc4345af8f0e293e19ef54cf4e5
Thomas Maximini <tmaximini@gmail.com>
2025-08-12 09:07:52 -0600

    remove news template

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,7 +11,6 @@ exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   const photosTemplate = require.resolve("./src/templates/photos.js")
-  const newsTemplate = require.resolve("./src/templates/news.js")
 
   const allPhotos = await wrapper(
     graphql(`
@@ -26,34 +25,10 @@ exports.createPages = async ({ graphql, actions }) => {
     `)
   )
 
-  const allNews = await wrapper(
-    graphql(`
-      {
-        news: allNewsYaml {
-          nodes {
-            slug
-            images
-          }
-        }
-      }
-    `)
-  )
-
   allPhotos.data.photos.nodes.forEach(node => {
     createPage({
       path: node.slug,
       component: photosTemplate,
-      context: {
-        slug: node.slug,
-        images: `/${node.images}/`,
-      },
-    })
-  })
-
-  allNews.data.news.nodes.forEach(node => {
-    createPage({
-      path: node.slug,
-      component: newsTemplate,
       context: {
         slug: node.slug,
         images: `/${node.images}/`,
